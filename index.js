026fd57d58cc0b3adc977bb41c3c24dead6e1783
Saustin-hash <67472303+Saustin-hash@users.noreply.github.com>
2020-09-20 13:55:57 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// index.js
+const express  = require('express');
+const mongoose = require('mongoose');
+
+const port = process.env.PORT        || 3000;
+const db   = process.env.MONGODB_URI || 'mongodb://localhost/hellodb';
+
+const app = express();
+
+// conexion a la base de datos
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log(`DB connected @ ${db}`);
+  })
+  .catch(err => console.error(`Connection error ${err}`));
+
+// listen
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
